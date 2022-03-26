5efdeb538396ae6642864d11d853c5af67ca3965
Anatoly <anatolykostarev@gmail.com>
2022-03-27 00:45:19 +0300

    Fix Nav

--- client/src/ui/navigation/navigation.jsx
+++ client/src/ui/navigation/navigation.jsx
@@ -1,26 +1,30 @@
 import React from "react";
 import s from "./navigation.module.css";
-import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from "reactstrap";
+import { Navbar, NavbarBrand, Nav, NavItem } from "reactstrap";
 import { Link } from "react-router-dom";
 import { navItems } from "../../data.js";
 
-const Navigation = () => {
+export const Navigation = () => {
   return (
     <div className={s.navigation}>
-      <Navbar className={s.navigation__navbar} expand="md" container="md">
+      <Navbar
+        className={s.navigation__navbar}
+        expand="md"
+        container="md"
+        color="light"
+      >
         <NavbarBrand className={s.navigation__navbrand} href="/">
           <span style={{ color: "#3399FF" }}>pi</span>
           <span style={{ color: "#F08080" }}>ki</span>
           <span style={{ color: "#33CC33" }}>mo</span>
           <span style={{ color: "#FF9900" }}>ni</span>
         </NavbarBrand>
-        <Nav>
+
+        <Nav className={s.list}>
           {navItems.map((elem, index) => (
             <NavItem className={s.navigation__navitem} key={index}>
-              <Link to={elem.href} style={{ textDecoration: "none" }}>
-                <NavLink className={s.navigation__navlink}>
-                  {elem.title}
-                </NavLink>
+              <Link to={elem.href}>
+                <div className={s.navigation__navlink}>{elem.title}</div>
               </Link>
             </NavItem>
           ))}
@@ -29,5 +33,3 @@ const Navigation = () => {
     </div>
   );
 };
-
-export default Navigation;
