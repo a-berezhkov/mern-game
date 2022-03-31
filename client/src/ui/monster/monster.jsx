034170789df0f186791adcd5fabcd208e739d4cc
Anatoly <anatolykostarev@gmail.com>
2022-03-31 23:22:04 +0300

    Create Monster component, add styles

--- client/src/ui/monster/monster.jsx
+++ client/src/ui/monster/monster.jsx
@@ -12,19 +12,23 @@ export const Monster = () => {
       <div className={s.monster}>
         <div className={s.frame}></div>
         <div className={s.parts}>
-          {monster.map((body) => (
-            <Draggable
-              axis="both"
-              key={body.id}
-              nodeRef={nodeRef}
-              onStart={(e) => e.preventDefault()}
-              onStop={(e) => console.log(e.pageX, e.pageY)}
-            >
-              <div id={body.id} className={s.monster__body} ref={nodeRef}>
-                <img src={body.body} alt="" />
-              </div>
-            </Draggable>
-          ))}
+          {monster.map((elem) =>
+            Object.values(elem).map((item) =>
+              item.map((el) => (
+                <Draggable
+                  axis="both"
+                  key={el.id}
+                  nodeRef={nodeRef}
+                  onStart={(e) => e.preventDefault()}
+                  onStop={(e) => console.log(e.pageX, e.pageY)}
+                >
+                  <div id={el.id} className={s.monster__body} ref={nodeRef}>
+                    <img src={el.url} alt="" />
+                  </div>
+                </Draggable>
+              ))
+            )
+          )}
         </div>
       </div>
     </div>
